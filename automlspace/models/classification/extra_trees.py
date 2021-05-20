f93340eeaff4199672163532e3ddf7aa5111c6fc
sy <459240868@qq.com>
2021-05-21 00:48:16 +0800

    update extra_trees

--- automlspace/models/classification/extra_trees.py
+++ automlspace/models/classification/extra_trees.py
@@ -1,68 +1,71 @@
-from ConfigSpace.configuration_space import ConfigurationSpace
-from ConfigSpace.hyperparameters import UniformFloatHyperparameter, \
-    UniformIntegerHyperparameter, CategoricalHyperparameter, UnParametrizedHyperparameter
-
-
 from ConfigSpace import ConfigurationSpace
 from ConfigSpace.hyperparameters import UniformFloatHyperparameter, UniformIntegerHyperparameter, \
     CategoricalHyperparameter, UnParametrizedHyperparameter
 from automlspace.models.utils import check_none, check_for_bool
 
 
-class ExtraTrees(object):
-    def __init__(self, n_estimators, criterion, min_samples_leaf,
-                 min_samples_split,  max_features, bootstrap, max_leaf_nodes,
-                 max_depth, min_weight_fraction_leaf, min_impurity_decrease,
-                 oob_score=False, n_jobs=1, random_state=None, verbose=0,
+class ExtraTrees:
+    def __init__(self, n_estimators, criterion, max_features,
+                 max_depth, min_samples_split, min_samples_leaf,
+                 min_weight_fraction_leaf, bootstrap, max_leaf_nodes,
+                 min_impurity_decrease, random_state=None, n_jobs=4,
                  class_weight=None):
-
         self.n_estimators = n_estimators
         self.criterion = criterion
-
-        if check_none(max_depth):
-            self.max_depth = None
-        else:
-            self.max_depth = int(max_depth)
-        if check_none(max_leaf_nodes):
-            self.max_leaf_nodes = None
-        else:
-            self.max_leaf_nodes = int(max_leaf_nodes)
-
-        self.min_samples_leaf = int(min_samples_leaf)
-        self.min_samples_split = int(min_samples_split)
-        self.max_features = float(max_features)
-        self.bootstrap = check_for_bool(bootstrap)
-        self.min_weight_fraction_leaf = float(min_weight_fraction_leaf)
-        self.min_impurity_decrease = float(min_impurity_decrease)
-        self.oob_score = oob_score
-        self.n_jobs = int(n_jobs)
+        self.max_features = max_features
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
+        self.min_weight_fraction_leaf = min_weight_fraction_leaf
+        self.bootstrap = bootstrap
+        self.max_leaf_nodes = max_leaf_nodes
+        self.min_impurity_decrease = min_impurity_decrease
         self.random_state = random_state
-        self.verbose = int(verbose)
+        self.n_jobs = n_jobs
         self.class_weight = class_weight
         self.estimator = None
 
-    def iterative_fit(self, X, y):
-        from sklearn.ensemble import ExtraTreesClassifier as ETC
+    def fit(self, X, y, sample_weight=None):
+        from sklearn.ensemble import ExtraTreesClassifier
+        self.n_estimators = int(self.n_estimators)
+        self.max_depth = int(self.max_depth)
 
-        max_features = int(X.shape[1] ** float(self.max_features))
-        self.estimator = ETC(n_estimators=self.n_estimators,
-                             criterion=self.criterion,
-                             max_depth=self.max_depth,
-                             min_samples_split=self.min_samples_split,
-                             min_samples_leaf=self.min_samples_leaf,
-                             bootstrap=self.bootstrap,
-                             max_features=max_features,
-                             max_leaf_nodes=self.max_leaf_nodes,
-                             min_weight_fraction_leaf=self.min_weight_fraction_leaf,
-                             min_impurity_decrease=self.min_impurity_decrease,
-                             oob_score=self.oob_score,
-                             n_jobs=self.n_jobs,
-                             verbose=self.verbose,
-                             random_state=self.random_state,
-                             class_weight=self.class_weight,
-                             warm_start=True)
+        self.min_samples_split = int(self.min_samples_split)
+        self.min_samples_leaf = int(self.min_samples_leaf)
+        self.min_weight_fraction_leaf = float(self.min_weight_fraction_leaf)
 
-        self.estimator.fit(X, y)
+        if self.max_features not in ("sqrt", "log2", "auto"):
+            max_features = int(X.shape[1] ** float(self.max_features))
+        else:
+            max_features = self.max_features
+
+        self.bootstrap = check_for_bool(self.bootstrap)
+
+        if check_none(self.max_leaf_nodes):
+            self.max_leaf_nodes = None
+        else:
+            self.max_leaf_nodes = int(self.max_leaf_nodes)
+
+        self.min_impurity_decrease = float(self.min_impurity_decrease)
+
+        # initial fit of only increment trees
+        self.estimator = ExtraTreesClassifier(
+            n_estimators=self.n_estimators,
+            criterion=self.criterion,
+            max_features=max_features,
+            max_depth=self.max_depth,
+            min_samples_split=self.min_samples_split,
+            min_samples_leaf=self.min_samples_leaf,
+            min_weight_fraction_leaf=self.min_weight_fraction_leaf,
+            bootstrap=self.bootstrap,
+            max_leaf_nodes=self.max_leaf_nodes,
+            min_impurity_decrease=self.min_impurity_decrease,
+            random_state=self.random_state,
+            n_jobs=self.n_jobs,
+            class_weight=self.class_weight,
+            warm_start=True)
+
+        self.estimator.fit(X, y, sample_weight=sample_weight)
         return self
 
     def predict(self, X):
@@ -70,41 +73,57 @@ class ExtraTrees(object):
             raise NotImplementedError
         return self.estimator.predict(X)
 
-    def predict_proba(self, X):
-        if self.estimator is None:
-            raise NotImplementedError()
-        probas = self.estimator.predict_proba(X)
-        return probas
-
     @staticmethod
-    def get_hyperparameter_search_space(dataset_properties=None, optimizer='smac'):
+    def get_hyperparameter_search_space(space_size='large'):
         cs = ConfigurationSpace()
+        if space_size == 'large':
+            n_estimators = UniformIntegerHyperparameter('n_estimators', 50, 800, default_value=100)
+            max_depth = UniformIntegerHyperparameter("max_depth", 4, 12, default_value=5)
+            max_features = UniformFloatHyperparameter("max_features", 0.3, 1., default_value=0.5)
 
-        criterion = CategoricalHyperparameter(
-            "criterion", ["gini", "entropy"], default_value="gini")
+            min_samples_leaf = UniformIntegerHyperparameter(
+                "min_samples_leaf", 1, 20, default_value=1)
+            min_samples_split = UniformIntegerHyperparameter(
+                "min_samples_split", 2, 20, default_value=2)
 
-        # The maximum number of features used in the forest is calculated as m^max_features, where
-        # m is the total number of features, and max_features is the hyperparameter specified below.
-        # The default is 0.5, which yields sqrt(m) features as max_features in the estimator. This
-        # corresponds with Geurts' heuristic.
-        max_features = UniformFloatHyperparameter(
-            "max_features", 0., 1., default_value=0.5)
+            bootstrap = CategoricalHyperparameter(
+                "bootstrap", ["True", "False"], default_value="True")
+            criterion = CategoricalHyperparameter(
+                "criterion", ["gini", "entropy"], default_value="gini")
 
-        max_depth = UnParametrizedHyperparameter(name="max_depth", value="None")
+            max_leaf_nodes = UnParametrizedHyperparameter("max_leaf_nodes", "None")
+            min_impurity_decrease = UnParametrizedHyperparameter('min_impurity_decrease', 0.0)
+            min_weight_fraction_leaf = UnParametrizedHyperparameter("min_weight_fraction_leaf", 0.)
+        elif space_size == 'medium':
+            n_estimators = UniformIntegerHyperparameter('n_estimators', 50, 800, default_value=100)
+            max_depth = UniformIntegerHyperparameter("max_depth", 4, 12, default_value=5)
 
-        min_samples_split = UniformIntegerHyperparameter(
-            "min_samples_split", 2, 20, default_value=2)
-        min_samples_leaf = UniformIntegerHyperparameter(
-            "min_samples_leaf", 1, 20, default_value=1)
-        min_weight_fraction_leaf = UnParametrizedHyperparameter('min_weight_fraction_leaf', 0.)
-        max_leaf_nodes = UnParametrizedHyperparameter("max_leaf_nodes", "None")
-        min_impurity_decrease = UnParametrizedHyperparameter('min_impurity_decrease', 0.0)
+            max_features = UniformFloatHyperparameter("max_features", 0.3, 1., default_value=0.5)
+            min_samples_leaf = UniformIntegerHyperparameter("min_samples_leaf", 1, 20, default_value=1)
+            min_samples_split = UniformIntegerHyperparameter("min_samples_split", 2, 20, default_value=2)
 
-        bootstrap = CategoricalHyperparameter(
-            "bootstrap", ["True", "False"], default_value="False")
-        cs.add_hyperparameters([criterion, max_features,
+            bootstrap = UnParametrizedHyperparameter("bootstrap", "True")
+            criterion = UnParametrizedHyperparameter("criterion", "gini")
+
+            max_leaf_nodes = UnParametrizedHyperparameter("max_leaf_nodes", "None")
+            min_impurity_decrease = UnParametrizedHyperparameter('min_impurity_decrease', 0.0)
+            min_weight_fraction_leaf = UnParametrizedHyperparameter("min_weight_fraction_leaf", 0.)
+        else:
+            n_estimators = UniformIntegerHyperparameter('n_estimators', 50, 800, default_value=100)
+            max_depth = UniformIntegerHyperparameter("max_depth", 4, 12, default_value=5)
+            max_features = UniformFloatHyperparameter("max_features", 0.3, 1., default_value=0.5)
+
+            min_samples_leaf = UnParametrizedHyperparameter("min_samples_leaf", 1)
+            min_samples_split = UnParametrizedHyperparameter("min_samples_split", 2)
+            bootstrap = UnParametrizedHyperparameter("bootstrap", "True")
+            criterion = UnParametrizedHyperparameter("criterion", "gini")
+
+            max_leaf_nodes = UnParametrizedHyperparameter("max_leaf_nodes", "None")
+            min_impurity_decrease = UnParametrizedHyperparameter('min_impurity_decrease', 0.0)
+            min_weight_fraction_leaf = UnParametrizedHyperparameter("min_weight_fraction_leaf", 0.)
+
+        cs.add_hyperparameters([n_estimators, criterion, max_features,
                                 max_depth, min_samples_split, min_samples_leaf,
                                 min_weight_fraction_leaf, max_leaf_nodes,
-                                min_impurity_decrease, bootstrap])
-
+                                bootstrap, min_impurity_decrease])
         return cs
